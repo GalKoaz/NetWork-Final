5b8f1b8721e9487170a653013bb2025d3db987ba
Gal <koazgal@gmail.com>
2022-05-17 18:04:07 +0300

    update files with commends

--- recv_udp.c
+++ recv_udp.c
@@ -19,33 +19,72 @@ void printsin(struct sockaddr_in *s, char *str1, char *str2) {
 }
 */
 
-int main(int argc, char *argv[])
-{
-  int socket_fd, cc, fsize;
-  struct sockaddr_in  s_in, from;
-  struct { char head; u_long  body; char tail;} msg;
+int main(int argc, char *argv[]) {
+    int socket_fd, cc, fsize;  // Socket number , cc , and file size
+    struct sockaddr_in s_in, from; // Socket destination and sources address
+    struct {
+        char head;
+        u_long body;
+        char tail;
+    } msg; // Struct : char of head unsigned number of body (The process number) and char tail.
 
-  socket_fd = socket (AF_INET, SOCK_DGRAM, 0);
+    socket_fd = socket(AF_INET, SOCK_DGRAM, 0);
+    /* Socket Creation with ( Internet domain , UDP , Protocol 0) */
 
-  bzero((char *) &s_in, sizeof(s_in));  /* They say you must do this    */
+    bzero((char *) &s_in, sizeof(s_in));
+    /* They say you must do this */
+    /*The bzero() function erases the data in the n bytes of the memory
+     starting at the location pointed to by s, by writing zeros (bytes
+    containing '\0') to that area.*/
 
-  s_in.sin_family = (short)AF_INET;
-  s_in.sin_addr.s_addr = htonl(INADDR_ANY);    /* WILDCARD */
-  s_in.sin_port = htons((u_short)0x3333);
+    s_in.sin_family = (short) AF_INET;
+    /* The address family for the transport address. This member should always be set to AF_INET. */
 
-  //printsin( &s_in, "RECV_UDP", "Local socket is:"); 
-  fflush(stdout);
+    s_in.sin_addr.s_addr = htonl(INADDR_ANY);
+    /* WILDCARD */
+    /* sin_addr is the IP address in the socket (the socket structure also contains other data, such as a port).
+    * The type of sin_addr is a union, so it can be accessed in three different ways : as s_un_b (four 1-byte integers),
+    * s_un_w (two 2-bytes integers) or as s_addr (one 4-bytes integer).
+    * The htonl() function converts the unsigned integer hostlong from host byte order to network byte order.
+    * getpid() returns the process ID (PID) of the calling process.
+    * (This is often used by routines that generate unique temporary filenames.)
+    *  ---------------> We accept connections from ANY internet address <---------------
+    */
 
-  bind(socket_fd, (struct sockaddr *)&s_in, sizeof(s_in));
+    s_in.sin_port = htons((u_short) 0x3333);
+    /* The sin_port is a transport protocol port number.
+    * The htons() function translates a short integer from host byte order to network byte order.
+     * Parameter Description a. The unsigned short integer to be put into network byte order. */
 
-  for(;;) {
-    fsize = sizeof(from);
-    cc = recvfrom(socket_fd,&msg,sizeof(msg),0,(struct sockaddr *)&from,&fsize);
-    //printsin( &from, "recv_udp: ", "Packet from:");
-    printf("Got data ::%c%ld%c\n",msg.head,(long) ntohl(msg.body),msg.tail); 
+    //printsin( &s_in, "RECV_UDP", "Local socket is:");
     fflush(stdout);
-  }
-  
-  return 0;
+    /* fflush(stdout) is typically used for output stream only. Its purpose is to clear (or flush)
+     * the output buffer and move the buffered data to console (in case of stdout)
+     * or disk (in case of file output stream). Below is its syntax.*/
+
+    bind(socket_fd, (struct sockaddr *) &s_in, sizeof(s_in));
+    /* The bind() function binds a unique local name to the socket with descriptor socket.
+     * the bind use socket_fd and the address socket in and the size*/
+
+    for (;;) { // while TRUE
+        fsize = sizeof(from); // file size
+
+        cc = recvfrom(socket_fd, &msg, sizeof(msg), 0, (struct sockaddr *) &from, &fsize);
+        /*
+         *The recvfrom() calls are used to receive messages from a socket,
+         * and may be used to receive data on a socket whether or not it is connection-oriented.
+         */
+
+        //printsin( &from, "recv_udp: ", "Packet from:");
+        printf("Got data ::%c%ld%c\n", msg.head, (long) ntohl(msg.body), msg.tail);
+        // print "Got data::< id process >"
+
+        fflush(stdout);
+        /* fflush(stdout) is typically used for output stream only. Its purpose is to clear (or flush)
+        * the output buffer and move the buffered data to console (in case of stdout)
+        * or disk (in case of file output stream). Below is its syntax.*/
+    }
+
+    return 0;
 }
 
--- send_udp.c
+++ send_udp.c
@@ -7,26 +7,58 @@
 #include <strings.h>
 #include <unistd.h>
 
-int main(int argc, char *argv[])
-{
-  int socket_fd;
-  struct sockaddr_in  dest;
-  struct hostent *hostptr;
-  struct { char head; u_long body; char tail; } msgbuf;
+int main(int argc, char *argv[]) {
+    int socket_fd; // unique identifier (handle) for a file or other input/output resource
+    struct sockaddr_in dest; // Socket destination address
+    struct hostent *hostptr; // Host pointer
+    struct {
+        char head;
+        u_long body;
+        char tail;
+    } msgbuf; // Struct : char of head unsigned number of body (The process number) and char tail.
 
-  socket_fd = socket (AF_INET, SOCK_DGRAM, 0);
-  bzero((char *) &dest, sizeof(dest)); /* They say you must do this */
-  hostptr = gethostbyname(argv[1]);
-  dest.sin_family = (short) AF_INET;
-  bcopy(hostptr->h_addr, (char *)&dest.sin_addr,hostptr->h_length);
-  dest.sin_port = htons((u_short)0x3333);
+    socket_fd = socket(AF_INET, SOCK_DGRAM, 0);
+    /* Socket Creation with ( Internet domain , UDP , Protocol 0) */
 
-  msgbuf.head = '<';
-  msgbuf.body = htonl(getpid()); /* IMPORTANT! */
-  msgbuf.tail = '>';
+    bzero((char *) &dest, sizeof(dest));
+    /* They say you must do this */
+    /*The bzero() function erases the data in the n bytes of the memory
+     starting at the location pointed to by s, by writing zeros (bytes
+    containing '\0') to that area.*/
 
-  sendto(socket_fd,&msgbuf,sizeof(msgbuf),0,(struct sockaddr *)&dest,
-                  sizeof(dest));
+    hostptr = gethostbyname(argv[1]);
+    /* The gethostbyname() function returns a structure of type hostent for the given host name. */
 
-  return 0;
+    dest.sin_family = (short) AF_INET;
+    /* The address family for the transport address. This member should always be set to AF_INET. */
+
+    bcopy(hostptr->h_addr, (char *) &dest.sin_addr, hostptr->h_length);
+    /* The bcopy() function copies n bytes from src to dest.  The result is correct, even when both areas overlap */
+
+    dest.sin_port = htons((u_short) 0x3333);
+    /* The sin_port is a transport protocol port number.
+     * The htons() function translates a short integer from host byte order to network byte order.
+     * Parameter Description a. The unsigned short integer to be put into network byte order. */
+
+    msgbuf.head = '<'; // define the head
+
+    msgbuf.body = htonl(getpid());
+    /* IMPORTANT! */
+    /* define the body:
+     * The htonl() function converts the unsigned integer hostlong from host byte order to network byte order.
+     * getpid() returns the process ID (PID) of the calling process.
+     * (This is often used by routines that generate unique temporary filenames.)
+     */
+
+    msgbuf.tail = '>'; // define the tail
+
+    sendto(socket_fd, &msgbuf, sizeof(msgbuf), 0, (struct sockaddr *) &dest, sizeof(dest));
+    /* sendto() used to transmit a message to another socket.
+     * sendto() is used on a connection-mode (SOCK_STREAM, SOCK_SEQPACKET) socket, the arguments
+     * dest_addr and addrlen are ignored (and the error EISCONN may be returned when they are not NULL and 0),
+     * and the error ENOTCONN is returned when the socket was not actually connected. Otherwise,
+     * the address of the target is given by dest_addr with addrlen specifying its size. For sendmsg(),
+     * the address of the target is given by msg.msg_name, with msg.msg_namelen specifying its size.
+     */
+    return 0;
 }
